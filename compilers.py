8ad58966c3208c250efa81015e386042991be9b7
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2020-03-05 22:07:07 +0700

    Update compilers.py

--- compilers.py
+++ compilers.py
@@ -4,8 +4,8 @@
 # Coded by Nedi Senja
 # Github: https://github.com/stepbystepexe/Compilers
 
-from __opt__ import *
-import os, sys, time, marshal
+import io, os, sys, time, random, marshal
+from time import sleep
 
 info = """
 Nama        : Compilers
@@ -25,124 +25,149 @@ NB          : Manusia gax ada yang sempurna
 
 [ \033[4mGunakan tool ini dengan bijak \033[0m]\n """
 
-example = """\033[106;90;1m[       Python Compilers, My Github: @stepbystepexe      ]\033[0m"""
+example = """\033[106;90;1m[          Compilers, My Github: @stepbystepexe          ]\033[0m"""
+
+logo = """
+ \033[0;91m█▀▀▀▀ █▀▀▀█ █▀█▀█ █▀▀▀█ ▀▀█▀▀ █     █▀▀▀▀   \033[0;37m█▀▀▀█ █   █
+ \033[0;91m█     █   █ █ ▀ █ █▀▀▀▀   █   █     █▀▀▀    \033[0;37m█▀▀▀▀ ▀▀█▀▀
+ \033[0;91m▀▀▀▀▀ ▀▀▀▀▀ ▀   ▀ ▀     ▀▀▀▀▀ ▀▀▀▀▀ ▀▀▀▀▀ \033[0;90m▀ \033[0;37m▀       ▀
+"""
 
 def restart():
     python = sys.executable
     os.execl(python, python, * sys.argv)
     curdir = os.getcwd()
 
+def loads():
+    o = [
+     '.   ','..  ','... ']
+    for i in o:
+        print '\r\033[0m[\033[0;33m●\033[0m] Sedang mengcompile ' +i,
+        sys.stdout.flush()
+        time.sleep(1)
+
+def write(o):
+    for i in o + '\n':
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+def home():
+    os.system('clear')
+    os.system('reset')
+    sleep(1)
+    print
+    print(logo)
+    print(example)
+    print
+    write("\033[0m[ \033[32mINFO \033[0m] \033[3mFile harus berada dalam satu folder yang sama")
+    write("         Jikalau tidak ya otomatis gx bisa dicompile lah\033[0m\n")
+
 def python3():
         try:
+                home()
+                sch = input("\n\033[0m[\033[41;77;1m File \033[0m] ")
+                exe = open(sch).read()
+                bom = compile(exe,'','exec')
+                cif = marshal.dumps(bom)
+                die = open("save."+sch,'w')
+                die.write('import marshal\n')
+                die.write('exec(marshal.loads('+repr(cif)+'))')
+                die.close()
                 print
-                a=input("\033[0m[\033[92;1m+\033[0m] \033[77;1mMasukan nama berkas: \033[0m")
-                x=open(a).read()
-                b=compile(x,'','exec')
-                c=marshal.dumps(b)
-                d=open("theresults_"+a,'w')
-                d.write('import marshal\n')
-                d.write('exec(marshal.loads('+repr(c)+'))')
-                d.close()
-                time.sleep(1.5)
-                print
-                print("\033[0m[\033[94;1m@\033[0m] \033[77;1mBerkas berhasil dicompile: \033[0mcompile_"+a)
-                print
-        except KeyboardInterrupt:
-                print
-                print("\n\033[0m[\033[91;1m!\033[0m] \033[77;1mGagal. [ INFO ] berkas harus berada dalam satu folder")
-                print
+                loads()
+                sleep(1)
+                print("\n\033[0m[\033[94;1m#\033[0m] \033[77;1mBerhasil dicompile: \033[0msave."+sch+"\n")
+                o = raw_input("\033[0m[\033[96;1m?\033[0m] Buka file [Y/n]: ")
+                if o.strip() in 'Y y'.split():
+                    print
+                    os.system('nano save.'+sch)
+                    print
+                    sys.exit(1)
+                elif o.strip() in 'N n'.split():
+                    restart()
+                else:
+                    restart()
+        except IOError:
+                print("\n\033[0m[\033[91;1m!\033[0m] \033[77;1mGagal mengcompile\033[0m\n")
                 sys.exit(1)
 def python2():
         try:
+                home()
+                sch = raw_input("\n\033[0m[\033[41;77;1m File \033[0m] ")
+                exe = open(sch).read()
+                bom = compile(exe,'','exec')
+                cif = marshal.dumps(bom)
+                die = open("save."+sch,'w')
+                die.write('import marshal\n')
+                die.write('exec(marshal.loads('+repr(cif)+'))')
+                die.close()
                 print
-                a=raw_input("\033[0m[\033[92;1m+\033[0m] \033[77;1mMasukan nama berkas: \033[0m")
-                x=open(a).read()
-                b=compile(x,'','exec')
-                c=marshal.dumps(b)
-                d=open("theresults_"+a,'w')
-                d.write('import marshal\n')
-                d.write('exec(marshal.loads('+repr(c)+'))')
-                d.close()
-                time.sleep(1.5)
-                print
-                print("\033[0m[\033[94;1m@\033[0m] \033[77;1mBerkas berhasil dicompile: \033[0mcompile_"+a)
-                print
-        except KeyboardInterrupt:
-                print
-                print("\n\033[0m[\033[91;1m!\033[0m] \033[77;1mGagal. [ INFO ] berkas harus berada dalam satu folder")
-                print
-                sys.exit(1)
-def python():
-        try:
-                print
-                a=raw_input("\033[0m[\033[92;1m+\033[0m] \033[77;1mMasukan nama berkas: \033[0m")
-                x=open(a).read()
-                b=compile(x,'','exec')
-                c=marshal.dumps(b)
-                d=open("theresults_"+a,'w')
-                d.write('import marshal\n')
-                d.write('exec(marshal.loads('+repr(c)+'))')
-                d.close()
-                time.sleep(1.5)
-                print
-                print("\033[0m[\033[94;1m@\033[0m] \033[77;1mBerkas berhasil dicompile: \033[0mcompile_"+a)
-                print
-        except KeyboardInterrupt:
-                print
-                print("\n\033[0m[\033[91;1m!\033[0m] \033[77;1mGagal. [ INFO ] berkas harus berada dalam satu folder")
-                print
+                loads()
+                sleep(1)
+                print("\n\033[0m[\033[94;1m#\033[0m] \033[77;1mBerhasil dicompile: \033[0msave."+sch+"\n")
+                o = raw_input("\033[0m[\033[96;1m?\033[0m] Buka file [Y/n]: ")
+                if o.strip() in 'Y y'.split():
+                    print
+                    os.system('nano save.'+sch)
+                    print
+                    sys.exit(1)
+                elif o.strip() in 'N n'.split():
+                    restart()
+                else:
+                    restart()
+        except IOError:
+                print("\n\033[0m[\033[91;1m!\033[0m] \033[77;1mGagal mengcompile\033[0m")
                 sys.exit(1)
 
 os.system('clear')
 os.system('reset')
-time.sleep(1)
+sleep(1)
 print
 print(logo)
 print(example)
 print
 print("\033[0m[\033[96;2;1m1\033[0m] \033[77;1mPython3.7")
 print("\033[0m[\033[96;2;1m2\033[0m] \033[77;1mPython2.7")
-print("\033[0m[\033[96;2;1m3\033[0m] \033[77;1mPython")
 print
 print("\033[0m[\033[93;1m&\033[0m] LISENSI")
 print("\033[0m[\033[94;1m#\033[0m] Informasi")
 print("\033[0m[\033[92;1m*\033[0m] Perbarui")
 print("\033[0m[\033[91;1m-\033[0m] Keluar")
 print
-option = raw_input("\n\033[0m[\033[95;1m/\033[0m] \033[77;1mMasukan opsi: \033[0m")
-if option == '1' or option == '01':
+option = raw_input("\033[0m[\033[95;1m/\033[0m] \033[77;1mMasukan opsi: \033[0m")
+if option.strip() in '1 python3'.split():
+    write("\n\033[0m[\033[32m●\033[0m] \033[77;1mSedang memproses ...\033[0m")
+    sleep(1)
     python3()
-elif option == '2' or option == '02':
+elif option.strip() in '2 python2'.split():
+    write("\n\033[0m[\033[32m●\033[0m] \033[77;1mSedang memproses ...\033[0m")
+    sleep(1)
     python2()
-elif option == '3' or option == '03':
-    python()
-elif option.strip() in '& 4 lisensi'.split():
+elif option.strip() in '& 3 lisensi'.split():
     print
     os.system('nano LICENSE')
     print
     restart()
-elif option.strip() in '# 5 info'.split():
+elif option.strip() in '# 4 info'.split():
     os.system('clear')
     print(example)
     os.system('toilet -f smslant Marshal')
     print(info)
-    time.sleep(1)
+    sleep(1)
     print
     raw_input('[ Tekan Enter ]')
     restart()
-elif option.strip() in '* 6 perbarui'.split():
+elif option.strip() in '* 5 perbarui'.split():
     print
     os.system('git pull origin master')
     print
-    raw_input('\033[0m[ \033[92mTekan Enter \033[0m]')
+    raw_input('\033[0m[ \033[32mTekan Enter \033[0m]')
     restart()
-elif option.strip() in '- 7 keluar'.split():
-    print
-    print("\033[0m[\033[91;1m!\033[0m] \033[77;1mKeluar dari program!")
-    print
+elif option.strip() in '- 0 keluar'.split():
+    print("\n\033[0m[\033[91;1m!\033[0m] \033[77;1mKeluar dari program!\n")
     sys.exit(1)
 else:
-    print("\n\033[0m[=\033[1;41;77m Pilihan Salah \033[0m=]")
-    print
-    time.sleep(1)
+    print("\n\033[0m[=\033[1;41;77m Pilihan Salah \033[0m=]\n")
+    sleep(1)
     restart()
